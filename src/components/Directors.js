8a359092271bafbc4447b9e96e1dd26f7d5c4d4d
jomapormentilla <jomapormentilla@gmail.com>
2021-03-01 18:15:09 -0500

    commit tree 2dc8430fd3419914e8afbc9cdbf85488b03fe909

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,20 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+  console.log(directors)
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      { directors.map(director => {
+        return(
+          <div className="director">
+            <h3>Name: { director.name }</h3>
+            <p>Movies:
+              { director.movies.map(movie => <li key={ movie }>{ movie }</li>) }
+            </p>
+          </div>
+        )
+      }) }
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,21 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  console.log( movies )
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        { movies.map(movie => {
+          return(
+            <div className="movie">
+            <h3>Name: { movie.title }</h3>
+            <p>Time: { movie.time }</p>
+            <p>Genres:
+              { movie.genres.map(genre => <li key={ genre }>{ genre }</li>) }
+            </p>
+          </div>
+          )
+        }) }
     </div>
   );
 };
